92108c41a2f6c0f86c736f6a7790df233e2e909e
Beppe Karlsson <beppe@live.se>
2016-09-13 14:38:56 +0200

    add profile component add to app.module add service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from './app.routing';
 
 import { AppComponent }  from './app.component';
+import { ProfileComponent } from './components/profile.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, routing ],
+  declarations: [ AppComponent,
+                  ProfileComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/app.routing.ts
@@ -0,0 +1,13 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProfileComponent } from './components/profile.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: ProfileComponent
+    }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
